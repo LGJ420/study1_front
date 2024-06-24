f3311aacc158cdc00222055c1264309879322523
unknown <leegj420@gmail.com>
2024-06-24 15:40:07 +0900

    2.9.1 페이지 추가

---
+++ src/pages/todo/ReadPage.js
@@ -0,0 +1,10 @@
+const ReadPage = () => {
+
+    return (
+        <div className="text-3xl font-extrabold">
+            Todo Read Page Component
+        </div>
+    );
+}
+
+export default ReadPage;
