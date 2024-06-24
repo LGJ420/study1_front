211601ab66b0dc2a18ab59c7a7500e936722e842
unknown <leegj420@gmail.com>
2024-06-24 15:13:02 +0900

    2.6 하위 경로의 설정과 <Outlet>

--- src/router/root.js
+++ src/router/root.js
@@ -4,6 +4,7 @@ import { createBrowserRouter } from "react-router-dom";
 const Loading = <div>Loading...</div>
 const Main = lazy(()=>import("../pages/MainPage"))
 const About = lazy(()=>import("../pages/AboutPage"))
+const TodoIndex = lazy(()=>import("../pages/todo/IndexPage"))
 
 const root = createBrowserRouter([
 
@@ -15,6 +16,11 @@ const root = createBrowserRouter([
     {
         path: "about",
         element: <Suspense fallback={Loading}><About /></Suspense>
+    },
+
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex /></Suspense>
     }
 
 ])
