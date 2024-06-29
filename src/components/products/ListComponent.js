cb4985d0f05d80a030dd2309427e6c5411303bcc
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-06-29 21:00:40 +0900

    8.5.2 Refresh Token을 이용한 자동 갱신

--- src/components/products/ListComponent.js
+++ src/components/products/ListComponent.js
@@ -4,6 +4,7 @@ import { getList } from "../../api/productsApi";
 import FetchingModal from "../common/FetchingModal";
 import { API_SERVER_HOST } from "../../api/todoApi";
 import PageComponent from "../common/PageComponent";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 const initState = {
     dtoList: [],
@@ -22,6 +23,8 @@ const host = API_SERVER_HOST;
 
 const ListComponent = () => {
 
+    const { exceptionHandle } = useCustomLogin();
+
     const {page, size, refresh, moveToList, moveToRead} = useCustomMove();
 
     const [serverData, setServerData] = useState(initState);
@@ -37,7 +40,7 @@ const ListComponent = () => {
             console.log(data);
             setServerData(data);
             setFetching(false);
-        })
+        }).catch(err=>exceptionHandle(err));
     }, [page, size, refresh]);
 
     return (
--- src/util/jwtUtil.js
+++ src/util/jwtUtil.js
@@ -1,9 +1,25 @@
 //Axios의 요청/응답 시에 동작할 함수들을 정의
 
 import axios from "axios"
-import { getCookie } from "./cookieUtil";
+import { getCookie, setCookie } from "./cookieUtil";
+import { API_SERVER_HOST } from "../api/todoApi";
 
 const jwtAxios = axios.create();
+
+const refreshJWT = async (accessToken, refreshToken) => {
+
+    const host = API_SERVER_HOST;
+
+    const header = {header: {"Authorization": `Bearer ${accessToken}`}};
+
+    //POST방식이 낫긴하지만 예제에서는 눈에 보이도록 GET방식을 이용했음
+    const res = await axios.get(`${host}/api/member/refresh?refreshToken=${refreshToken}`, header);
+
+    console.log("----------------------");
+    console.log(res.data);
+
+    return res.data;
+}
 
 const beforeReq = (config) => {
 
@@ -40,6 +56,31 @@ const requestFail = (err) => {
 const beforeRes = async (res) => {
 
     console.log("before return response.............")
+
+    const data = res.data;
+
+    if(data && data.error === 'ERROR_ACCESS_TOKEN'){
+
+        const memberCookieValue = getCookie("member");
+
+        const result = await refreshJWT(memberCookieValue.accessToken,
+            memberCookieValue.refreshToken);
+
+        console.log("refreshJWT RESULT", result);
+
+        memberCookieValue.accessToken = result.accessToken;
+        memberCookieValue.refreshToken = result.refreshToken;
+
+        setCookie("member", JSON.stringify(memberCookieValue), 1);
+
+
+        // 원래 요청에 새로받은 accessToken을 넣어서 다시 요청
+        const originalRequest = res.config;
+
+        originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+
+        return await axios(originalRequest);
+    }
 
     return res;
 }
