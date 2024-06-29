e46265b7ae394179e45609360b8622eaf8786fbf
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-06-29 19:58:37 +0900

    8.4.1 로그인 결과의 쿠키 보관

--- src/slices/loginSlice.js
+++ src/slices/loginSlice.js
@@ -1,8 +1,21 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { loginPost } from "../api/memberApi"
+import { getCookie, removeCookie, setCookie } from "../util/cookieUtil";
 
 const initState = {
     email: ''
+}
+
+const loadMemberCookie = () => {
+
+    const memberInfo = getCookie("member");
+
+    //닉네임 처리
+    if(memberInfo && memberInfo.nickname){
+        memberInfo.nickname = decodeURIComponent(memberInfo.nickname);
+    }
+
+    return memberInfo;
 }
 
 export const loginPostAsync = createAsyncThunk('loginPostAsync', (param)=>{
@@ -16,8 +29,9 @@ const loginSlice = createSlice({
 
     /**
      * 앞으로 스토어에서 관리할 상태값
+     * 쿠키가 있을경우 쿠키값을 사용하고 그렇지 않을때는 initState사용
      */
-    initialState: initState,
+    initialState: loadMemberCookie() || initState,
 
     /**
      * 그리고 그것을 처리하는 리듀서
@@ -25,6 +39,7 @@ const loginSlice = createSlice({
      * action creators가 자동으로 생성됨
      */
     reducers: {
+        //동기방식의 로그인이다
         login: (state, action) => {
             console.log("login.....");
 
@@ -39,15 +54,27 @@ const loginSlice = createSlice({
         logout: (state, action) => {
             console.log("logout....");
 
+            removeCookie("member");
+
             return {...initState};
         }
     },
     
+    //비동기 방식의 로그인을 만든다
     extraReducers: (builder) => {
         builder.addCase(loginPostAsync.fulfilled, (state, action)=>{
             console.log("fulfilled"); //완료
             const payload = action.payload;
+
+            //정상적인 로그인 시에만 쿠키에 저장
+            if(!payload.error){
+                
+                //유효기간 1일
+                setCookie("member", JSON.stringify(payload), 1);
+            }
+
             return payload;
+
         }).addCase(loginPostAsync.pending, (state, action)=>{
             console.log("pending"); //처리중
         }).addCase(loginPostAsync.rejected, (state, action)=>{
