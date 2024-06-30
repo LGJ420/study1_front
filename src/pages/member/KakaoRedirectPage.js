0202e95e33cffd02d42bece5bdc53cc39e965c2d
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-06-30 14:46:59 +0900

    9.3.2 Access Token 받기

--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { getAccessToken } from "../../api/kakaoApi";
 
 const KakaoRedirectPage = () => {
 
     const [searchParams] = useSearchParams();
 
     const authCode = searchParams.get("code");
+
+    useEffect(()=>{
+
+        getAccessToken(authCode).then(data=>{
+            console.log(data);
+        })
+    }, [authCode]);
 
     return (
         <div>
