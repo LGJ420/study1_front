b2a200a6dae750667c1e8a7b0e68c7f841c1b5b7
unknown <leegj420@gmail.com>
2024-07-01 10:40:27 +0900

    11.2.1 로그인 상태 체크와 장바구니

--- src/components/menus/CartComponent.js
+++ src/components/menus/CartComponent.js
@@ -1,8 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import useCustomLogin from "../../hooks/useCustomLogin";
+import { getCartItemsAsync } from "../../slices/cartSlice"
+import { useEffect } from "react";
+
 const CartComponent = () => {
+
+    const {isLogin, loginState} = useCustomLogin();
+
+    const dispatch = useDispatch();
+
+    const cartItems = useSelector(state => state.cartSlice);
+
+    useEffect(()=>{
+
+        if(isLogin){
+
+            dispatch(getCartItemsAsync());
+        }
+
+    },[isLogin]);
 
     return (
         <div className="w-full">
-            <div>Cart</div>
+            {isLogin ?
+            <div className="flex">
+                <div className="m-2 font-extrabold">
+                    {loginState.nickname}'s Cart
+                </div>
+                <div className="bg-orange-600 w-9 text-center text-white font-bold rounded-full m-2">
+                    {cartItems.length}
+                </div>
+            </div>
+            :
+            <div></div>
+            }
         </div>
     )
 }
